b92b8e8bef92d9dd154c8e07e30afad216517b35
NathanDondellBoyd <nathandondellboyd@gmail.com>
2021-03-09 13:07:31 -0500

    added reqired conditions, still working out javascript code

--- notary.js
+++ notary.js
@@ -1,5 +1,31 @@
+/*
 function test() {
-    test = alert ("Success!")
+    alert ("Success!")
 }
 
-test();
+test();*/
+
+
+/*function estimate( ){
+
+
+
+}
+console.log ( 'Your estimate is' + estimate() )*/
+
+/* Collects data from class = Client */
+const client = {
+    firstName: 'first_name',
+    middleInitial: 'middle_initial',
+    lastName: 'last_name',
+    address: 'address',
+    city: 'city',
+    state: 'state',
+    zip: 'zip',
+    time: 'time',
+    phone:'phone',
+    email: 'email',
+};
+console.log(client);
+
+
