78057e8e4a1c9a265be2228128a137da54aa497c
NathanDondellBoyd <nathandondellboyd@gmail.com>
2021-03-15 11:45:38 -0400

    Starting to work on Estimate Functions in JavaScript

--- notary.js
+++ notary.js
@@ -13,7 +13,7 @@ test();*/
 }
 console.log ( 'Your estimate is' + estimate() )*/
 
-/* Collects data from class = Client */
+/* Collects data from class = Client 
 const client = {
     firstName: 'first_name',
     middleInitial: 'middle_initial',
@@ -27,5 +27,54 @@ const client = {
     email: 'email',
 };
 console.log(client);
+*/
+
+function getPages(){
+    
+    let p = document.getElementById('pages').value;
+     p = parseInt(pages);
+    
+    
+}
+
+function getDocument(){
+    let d= document.getElementById('document').value;
+        if (document=="Personal")
+         { d=10;}
+          if (document=="Business")
+         { d= 25;}
+          if (document=="Real Estate")
+        { d= 50;}
+        
+}
+
+  function getEstimate(){
+        const theButton= document.getElementById('theButton');
+         let workTotal = getPages() + getDocument();
+
+         theButton.addEventListener('onclick',() => {
+
+         alert( workTotal.value)})
+
+        }
+
+ 
+
+    
+        
+        
+
+
+
+
+
+        
+
+
+
+
+
+
+
 
 
